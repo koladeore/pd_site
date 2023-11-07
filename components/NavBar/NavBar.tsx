a6d363364d7650a8542193519570ae8693257875
koladeore <koladeore@gmail.com>
2023-11-07 21:02:58 +0100

    home-body

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -7,7 +7,7 @@ import NavLinks from "./NavLinks";
 const NavBar = () => {
   const [open, setOpen] = useState(false);
   return (
-      <nav className="bg-white">
+      <nav className="bg-white fixed w-full">
       <div className="flex items-center font-medium justify-around">
         <div className="z-50 p-5 md:w-auto w-full flex justify-between items-center h-16">
           <Link href="/" className="">
