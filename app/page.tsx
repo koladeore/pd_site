9a4baa5b4fd4fdfafdd2b22b1650d162fb7b5fe6
koladeore <koladeore@gmail.com>
2023-11-13 00:26:20 +0100

    styling-audioPlayer-components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
 import AudioPlayer from "@/components/AudioPlayer/AudioPlayer";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <div className="bg-[#1f1f21] pt-16">
         <div className="flex md:justify-around items-center justify-center md:flex-row flex-col">
-          <div className="md:order-1 order-last">
+          <div className="md:order-1 order-last text-center sm:text-left mt-4">
             <h1 className="text-white font-bold md:text-4xl pb-5 text-2xl">Are you Responding to God&apos;s Love?</h1>
-            <p className="text-white leading-7 md:text-base text-sm">
+            <p className="text-white md:leading-7 leading-6 md:text-base text-sm">
               When you truly grasp the depth of God&apos;s love for you, it compels you to<br />
               respond in love towards others. It motivates you to extend grace, show<br />
               compassion, and seek reconciliation.God&apos;s love becomes a catalyst for<br />
@@ -27,6 +28,9 @@ export default function Home() {
         </div>
       </div>
       <AudioPlayer />
+      <div className="flex items-center justify-center pt-10 pb-8">
+        <Link href="/" className="bg-transparent text-pink-500 border border-pink-500 py-2 px-4 rounded inline-block hover:bg-pink-500 hover:text-white">More Messages</Link>
+      </div>
     </div>
   )
 }
--- components/AudioPlayer/AudioPlayer.tsx
+++ components/AudioPlayer/AudioPlayer.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useRef, useEffect } from "react";
 // import { BsArrowLeftShort } from 'react-icons/bs';
+import { BsCameraVideo } from 'react-icons/bs';
+import { SlEarphones } from 'react-icons/sl';
+import { BsCloudDownload } from 'react-icons/bs';
 import { client } from "@/app/lib/sanity";
 import { urlFor } from "@/app/lib/sanityImageUrl";
 import Image from "next/image";
@@ -12,6 +15,7 @@ interface AudioData {
   youtubeUrl: string;
  description: string;
 }
+const revalidate = 60;
 async function getData() {
   const query = `*[_type == 'audioMessage']{ 
   title,
@@ -20,41 +24,57 @@ async function getData() {
   'image': image.asset->url,
   youtubeUrl,
 }`;
-  const data = await client.fetch(query);
+  const data = await client.fetch(query, {
+    next: {
+      revalidate: 10
+    }
+  });
   return data;
 }
 const AudioPlayer = async () => {
   const data = (await getData()) as AudioData[];
   return (
-    <div>
-      {data.map((audio) => (
+    <div className="pl-32 pt-10 grid md:grid-cols-3">
+      {data.slice(0,3).map((audio) => (
         <div key={audio.title}>
-          <p>{audio.title}</p>
-          <p>{audio.description}</p>
-            <a href={audio.youtubeUrl} target="_blank" rel="noopener noreferrer">youtubeUrl</a>
           <Image
             src={urlFor(audio.image).url()}
             alt="Image"
-            className="rounded-lg"
-            width={400}
-            height={400}
+            className=""
+            width={250}
+            height={250}
           />
-          <div className="w-72">
+          <div className="flex gap-8 pt-4">
+            <a href={audio.youtubeUrl} target="_blank" rel="noopener noreferrer">
+              <BsCameraVideo className=""/>
+            </a>
             <div>
-              <audio controls>
-                <source src={audio.file} type="audio/mpeg" />
-              </audio>
-              <br />
+              {/* LISTEN ICON */}
+              <button>
+                <SlEarphones/>
+              </button>
+              {/* <div className="w-72">
+                <div>
+                  <audio controls>
+                    <source src={audio.file} type="audio/mpeg" />
+                  </audio>
+                </div>
+              </div> */}
+            </div>
+            <div>
               <a
-                href={audio.file}
-                download={`${audio.title}.mp3`}
-                target="_blank"
-                rel="noopener noreferrer"
+              href={audio.file}
+              download={`${audio.title}.mp3`}
+              target="_blank"
+              rel="noopener noreferrer"
               >
-                Download
+                <BsCloudDownload />
               </a>
             </div>
           </div>
+          <div className="border-t border-gray-300 h-0 w-64 mt-4"></div>
+          <p className="text-3xl font-bold text-pink-500 w-72 pt-8">{audio.title}</p>
+          <p className="pt-4 pb-10 text-gray-700 text-sm font-normal leading-6 w-72">{audio.description}</p>
         </div>
       ))}
     </div>
